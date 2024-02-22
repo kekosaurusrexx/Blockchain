8287b73b04b615f6f9d2af4506fd456d2b97aaa5
Tim <148331994+kekosaurusrexx@users.noreply.github.com>
2024-02-22 17:04:48 +0100

    Add files via upload

---
+++ console.py
@@ -0,0 +1,12 @@
+host = "127.0.0.1"
+port = 420
+
+while True:
+    inputstr = input()
+    try:
+        s = socket.socket()
+        s.connect((host, port))
+        s.send(inputstr.encode("UTF-8"))
+        s.close()
+    except:
+        print("Could not send command")
