a83a66979a0b0515a261064987a6946b72f84229
Tim <148331994+kekosaurusrexx@users.noreply.github.com>
2024-03-01 14:25:00 +0100

    Database for blockchain

---
+++ config.py
@@ -0,0 +1,10 @@
+#Configuration for this node
+miningdifficulty = 5            #Set mining difficulty
+blockchainfolder = "blockchain" #Set path to blockchain folder
+blockchaindb = "blockchain.db"  #Set path to blockchain database
+blockchaintable = "chain"       #Set name of blockchain table in database
+keyfile = "keys"                #Set name of key file
+socketServerHost = "127.0.0.1"  #Socket server host
+socketServerPort = 420          #Socket server port
+socketServerClients = 10        #Socket server max. clients
+debugMode = True                #Changing logging and functions
