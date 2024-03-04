2c00cb9990da6e0e85d6d639d2214daff791e226
Tim <148331994+kekosaurusrexx@users.noreply.github.com>
2024-03-04 09:34:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,26 @@
-#Configuration for this node
-miningdifficulty = 5            #Set mining difficulty
-blockchainfolder = "blockchain" #Set path to blockchain folder
-blockchaindb = "blockchain.db"  #Set path to blockchain database
-blockchaintable = "chain"       #Set name of blockchain table in database
-keyfile = "keys"                #Set name of key file
-socketServerHost = "127.0.0.1"  #Socket server host
-socketServerPort = 420          #Socket server port
-socketServerClients = 10        #Socket server max. clients
-debugMode = True                #Changing logging and functions
+import configparser
+from os.path import exists
+
+configfilename = "config.ini"
+debugMode = True
+
+config = configparser.ConfigParser()
+if not exists(configfilename):
+    config["DEFAULT"]["miningdifficulty"] = "5"
+    config["DEFAULT"]["blockchaindb"] = "blockchain.db"
+    config["DEFAULT"]["blockchaintable"] = "chain"
+    config["DEFAULT"]["keyfile"] = "miner.keys"
+    config["DEFAULT"]["socketServerHost"] = "localhost"
+    config["DEFAULT"]["socketServerPort"] = "420"
+    config["DEFAULT"]["socketServerClients"] = "10"
+    with open(configfilename, 'w') as configfile:
+        config.write(configfile)
+
+config.read(configfilename)
+miningdifficulty = int(config["DEFAULT"]["miningdifficulty"])       #Set mining difficulty
+blockchaindb = config["DEFAULT"]["blockchaindb"]                    #Set path to blockchain database
+blockchaintable = config["DEFAULT"]["blockchaintable"]              #Set name of blockchain table in database
+keyfile = config["DEFAULT"]["keyfile"]                              #Set name of key file
+socketServerHost = config["DEFAULT"]["socketServerHost"]            #Socket server host
+socketServerPort = int(config["DEFAULT"]["socketServerPort"] )      #Socket server port
+socketServerClients = int(config["DEFAULT"]["socketServerClients"]) #Socket server max. clients
